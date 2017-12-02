bfbccdf8b8b50bcd9e50ba79747a4e0ff8c5ceb3
ewfian <ewfian@vip.qq.com>
2017-12-02 19:41:28 +0800

    修改页面样式

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,1 +1,13 @@
 $(document).ready(function () {});
+
+calendar = function () {
+    // 返回指定月份的天数
+    this.getDaysOfMonth = function (year, month) {
+        return new Date(year, month, 0).getDate();
+    };
+
+    // 返回指定月份的第一天是一周中的第几天
+    this.getFirstWeekDayOfMonth = function (year, month) {
+        return new Date(year, month, 0).getDay();
+    };
+};
